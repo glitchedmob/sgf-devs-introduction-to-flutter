135223a9f999047e1904f80588898620a5278c6d
Levi Zitting <me@levizitting.com>
2020-05-05 22:01:40 -0500

    add missing routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+
 import 'package:intro_app/screens/basic_ui_widgets_screen.dart';
 import 'package:intro_app/screens/forms_and_inputs_screen.dart';
-
+import 'package:intro_app/screens/interacting_with_native_screen.dart';
 import 'package:intro_app/screens/landing_screen.dart';
 import 'package:intro_app/screens/build_in_widgets_screen.dart';
 import 'package:intro_app/screens/layout_widgets_screen.dart';
 import 'package:intro_app/screens/list_view_screen.dart';
+import 'package:intro_app/screens/networking_and_json.dart';
+import 'package:intro_app/screens/state_management_screen.dart';
 
 void main() {
   runApp(App());
 }
+
+var routes = {
+  '/landing': (context) => LandingScreen(),
+  '/built-in-widgets': (context) => BuiltInWidgetsScreen(),
+  '/built-in-widgets/basic-ui-widgets': (context) => BasicUiWidgetsScreen(),
+  '/built-in-widgets/layout-widgets': (context) => LayoutWidgetsScreen(),
+  '/built-in-widgets/list-view': (context) => ListViewScreen(),
+  '/built-in-widgets/forms-and-inputs': (context) => FormsAndInputsScreen(),
+  '/state-management': (context) => StateManagementScreen(),
+  '/networking-and-json': (context) => NetworkingAndJsonScreen(),
+  '/interacting-with-native': (context) => InteractingWithNativeScreen(),
+};
 
 class App extends StatelessWidget {
   @override
@@ -21,14 +36,7 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       initialRoute: '/landing',
-      routes: {
-        '/landing': (context) => LandingScreen(),
-        '/built-in-widgets': (context) => BuiltInWidgetsScreen(),
-        '/built-in-widgets/basic-ui-widgets': (context) => BasicUiWidgetsScreen(),
-        '/built-in-widgets/layout-widgets': (context) => LayoutWidgetsScreen(),
-        '/built-in-widgets/list-view': (context) => ListViewScreen(),
-        '/built-in-widgets/forms-and-inputs': (context) => FormsAndInputsScreen(),
-      },
+      routes: routes,
     );
   }
 }
