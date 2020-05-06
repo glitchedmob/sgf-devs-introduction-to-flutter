a630fbbf2f38b4354bb2dea6468bc840013bfdd3
Levi Zitting <me@levizitting.com>
2020-05-06 10:39:00 -0500

    Add FormsAndInputsScreen content

--- lib/screens/forms_and_inputs_screen.dart
+++ lib/screens/forms_and_inputs_screen.dart
@@ -1,13 +1,74 @@
 import 'package:flutter/material.dart';
 
-class FormsAndInputsScreen extends StatelessWidget {
+class FormsAndInputsScreen extends StatefulWidget {
+  @override
+  _FormsAndInputsScreenState createState() => _FormsAndInputsScreenState();
+}
+
+class _FormsAndInputsScreenState extends State<FormsAndInputsScreen> {
+  final textController = TextEditingController(text: 'default text');
+  final _formKey = GlobalKey<FormState>();
+  var formValue = '';
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Forms and Inputs'),
       ),
-      body: Container(),
+      body: ListView(
+        children: [
+          TextField(
+            controller: textController,
+            onChanged: (value) {
+              setState(() {});
+            },
+          ),
+          RaisedButton(
+            child: Text('submit'),
+            onPressed: () {
+              print(textController.text);
+            },
+          ),
+          Text(textController.text, style: TextStyle(fontSize: 20)),
+          SizedBox(height: 50),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  onSaved: (value) {
+                    print('saving');
+                    formValue = value;
+                  },
+                  validator: (value) {
+                    print('validating');
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+                RaisedButton(
+                  child: Text('submit'),
+                  onPressed: () {
+                    if(_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                      print('Form value: $formValue');
+                    }
+                  },
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
