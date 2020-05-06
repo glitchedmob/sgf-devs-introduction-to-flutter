08a9e9b7a2ae4d31671018e98598a2c110bfcef7
Levi Zitting <me@levizitting.com>
2020-05-05 23:13:15 -0500

    Add LayoutWidgetsScreen content

--- lib/screens/layout_widgets_screen.dart
+++ lib/screens/layout_widgets_screen.dart
@@ -4,10 +4,31 @@ class LayoutWidgetsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Layout Widgets'),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: Text('Layout Widgets'),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.lightGreen,
+            child: Row(
+              children: [
+                Text('test'),
+                Text('test'),
+                Flexible(
+                  flex: 1,
+                  child: Text('testtesttest'),
+                ),
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                    width: 20,
+                    color: Colors.blue,
+                  ),
+                ),
+                Text('test'),
+              ],
+            ),
+          ),
+        ));
   }
 }
